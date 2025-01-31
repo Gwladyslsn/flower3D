c3a7fc9d835679b9dffd386d03dbe8dd376b9443
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-01-31 17:18:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+projet :
+fleur en 3D qui réagit en fonction du niveau de vent par rapport à la ville recherché
---
+++ modules/Search.js
@@ -0,0 +1,17 @@
+const moduleName = "Search";
+
+/* SearchBar
+- récupère la ville renseigné par le visiteur
+- à partir du nom de ville, va aller trouver la latitude et 
+longitude correspondantes
+- Si la ville renseignée n'est pas dans la base, elle doit indiquer 
+l'erreur au visiteur 
+
+BONUS : 
+- Pourrait proposer des suggestions de ville au moment de la saisie (autocompletion)
+- Enregistrer les dernières recherches
+*/
+
+class Search {}
+
+export { moduleName };
